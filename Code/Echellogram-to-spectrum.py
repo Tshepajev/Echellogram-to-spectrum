c811996fd95d6e68d6dcd168389f5b05aa99a1e5
Tshepajev <79028775+Tshepajev@users.noreply.github.com>
2025-03-14 15:48:07 +0200

    Code outline

---
+++ Code/Echellogram-to-spectrum.py
@@ -0,0 +1,32 @@
+# Author: Jasper Ristkok
+
+# Code to convert an echellogram (photo) to spectrum
+
+def main():
+    
+    process_photos()
+    
+    return
+
+def process_photos():
+    average_photos()
+    
+    output_averaged_photo()
+    return
+
+
+def average_photos():
+    load_photo()
+    return
+
+def load_photo():
+    
+    return
+
+
+def output_averaged_photo():
+    
+    return
+
+
+main()
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Echellogram-to-spectrum
  Code to convert an echellogram (photo) to spectrum
+
+Author: Jasper Ristkok
+
+Licence: MIT - do whatever you want with the script. It's nice if you mention me if your work is largely based on my script but I won't come looking for you if you don't.
